34564bf9ec21253ba6e43575c2c29988dc7af468
martin.froehlich <registered@maybites.ch>
2014-01-25 16:00:00 +0100

    removed kinect client

--- Lib_maybitesMXJ/src/ch/maybites/mxj/utils/kinect/KinectClientListener.java
+++
@@ -1,8 +0,0 @@
-package ch.maybites.mxj.utils.kinect;
-
-public interface KinectClientListener {
-
-	public void udpevent(String info);
-	public void tcpevent(String info);
-	public void tcperror(String error);
-}
